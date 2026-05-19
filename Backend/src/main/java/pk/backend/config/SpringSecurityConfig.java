0616041aed98589a1789670d876f47a2eeb48691
PatMaz999 <Patryk.m.3456@gmail.com>
2026-05-19 14:03:42 +0200

    feat: implement security filter chain

---
+++ Backend/src/main/java/pk/backend/config/SpringSecurityConfig.java
@@ -0,0 +1,23 @@
+package pk.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                ).build();
+    }
+}
